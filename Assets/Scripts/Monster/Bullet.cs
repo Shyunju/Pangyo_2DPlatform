7e3cfcadd5d707ed791f1a1f9dfc7161726d4b50
Shyunju <hyunju9908@naver.com>
2023-10-13 19:09:13 +0900

    [CHORE]디테일 수정

---
+++ Assets/Scripts/Monster/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = new Vector2(-10, rigid.velocity.y);
+
+        Destroy(gameObject, 3f);
+    }
+
+    
+}
--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     private SpriteRenderer renderer;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rigid;
     private Animator anim;
     private int nextMove;
-    [SerializeField] private MonsterSO monsterSo;
+    [SerializeField] private MonsterSO monsterSO;
 
     private void Awake()
     {
         renderer = GetComponent<SpriteRenderer>();
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         Moving();
     }
@@ -22,17 +22,17 @@ public class Monster : MonoBehaviour
     void FixedUpdate()
     {
         //기본 동작
-        rigidbody.velocity = new Vector2(monsterSo.speed * nextMove, rigidbody.velocity.y);
+        rigid.velocity = new Vector2(monsterSO.speed * nextMove, rigid.velocity.y);
 
         //지형 감지 
-        Vector2 frontVec = new Vector2(rigidbody.position.x + nextMove * 0.3f, rigidbody.position.y);
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.3f, rigid.position.y);
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform")); //땅의 레이어가 Platform
         if(rayHit.collider == null)
         {
             nextMove *= -1;
             CancelInvoke();
-            Invoke(nameof(Moving), monsterSo.delay);
+            Invoke(nameof(Moving), monsterSO.delay);
         }
     }
 
@@ -41,17 +41,17 @@ public class Monster : MonoBehaviour
         nextMove = Random.Range(-1, 2);
         if(nextMove > 0) 
         {
-            anim.SetBool("isWalking", true);
+            //anim.SetBool("isWalking", true);
             renderer.flipX = true;
         }else if(nextMove < 0)
         {
             renderer.flipX = false;
-            anim.SetBool("isWalking", true);
+            //anim.SetBool("isWalking", true);
         }
         else
         {
-            anim.SetBool("isWalking", false);
+            //anim.SetBool("isWalking", false);
         }
-        Invoke(nameof(Moving), monsterSo.delay);
+        Invoke(nameof(Moving), monsterSO.delay);
     }
 }
