82ead726e47f826380957ebf2e4f85545b19030b
Shyunju <hyunju9908@naver.com>
2023-10-13 17:18:49 +0900

    [UPDATE] moster 지형감지 기능 추가

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -19,13 +19,25 @@ public class Monster : MonoBehaviour
 
     void FixedUpdate()
     {
+        //기본 동작
         rigidbody.velocity = new Vector2(monsterSo.speed * nextMove, rigidbody.velocity.y);
+
+        //지형 감지 
+        Vector2 frontVec = new Vector2(rigidbody.position.x + nextMove * 0.3f, rigidbody.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform")); //땅의 레이어가 Platform
+        if(rayHit.collider == null)
+        {
+            nextMove *= -1;
+            CancelInvoke();
+            Invoke(nameof(Moving), monsterSo.delay);
+        }
     }
 
     private void Moving()
     {
         nextMove = Random.Range(-1, 2);
-        if(nextMove > 0)
+        if(nextMove > 0) 
         {
             renderer.flipX = true;
         }else if(nextMove < 0)
