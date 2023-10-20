ad351f7ab435b9106f1fab87802730a93077abd4
Shyunju <hyunju9908@naver.com>
2023-10-20 16:31:45 +0900

    [CHORE] 주석 제거

--- Assets/Scripts/Monster/FireMonster.cs
+++ Assets/Scripts/Monster/FireMonster.cs
@@ -7,20 +7,12 @@ public class FireMonster : MonoBehaviour
     [SerializeField] private MonsterSO monsterSO;
     [SerializeField] private GameObject spawner;
     [SerializeField] private GameObject bullet;
-    // Start is called before the first frame update
+
     void Start()
     {
-        //bullet = GetComponent<GameObject>();
-        //spawner = GetComponent<GameObject>();
         MakeBullet();
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     void MakeBullet()
     {
         Instantiate(bullet, spawner.transform.position, spawner.transform.rotation);
--- Assets/Scripts/Monster/Rain.cs
+++ Assets/Scripts/Monster/Rain.cs
@@ -6,10 +6,6 @@ public class Rain : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if(collision.gameObject.layer == 6 || collision.gameObject.tag == "Player")
-        //{
-        //    Destroy(gameObject);
-        //}
         Destroy(gameObject);
     }
 }
